ac746e9faba487c3269d1bbe2a0c2dfb28919827
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-16 20:25:55 +0600

    implement jwt based authentication

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
+import { authRoute } from "./modules/auth/auth.route";
 
 const app: Application = express();
 
@@ -23,6 +24,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
 
 // GET all users from database
 
