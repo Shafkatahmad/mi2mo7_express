2c4839db74360e64dd4bf67f8462e92324ae964b
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-16 17:26:00 +0600

    separate code to specific user modules for GET, GET by id, PUT, DELETE methods

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -41,6 +41,134 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+// GET all users from db
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    // separated the DB query into the user.service module
+    const result = await userService.getAllUsersFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+// GET single user by id from db
+const getSingleUser = async (req: Request, res: Response) => {
+  // const id = req.params;
+  // const id = req.params.id;
+  const { id } = req.params;
+
+  try {
+    // separated the DB query into the user.service module
+    const result = await userService.getSingleUserFromDB(id as string);
+
+    // If user not found by id
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully",
+      data: result.rows[0],
+    });
+    console.log(result);
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+// Update user by PUT methon in db
+const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  // console.log("ID: ", id);
+  // console.log({ name, password, age, id_active });
+
+  try {
+    // separated the DB query into the user.service module
+    const result = await userService.updateUserIntoDB(req.body, id as string);
+
+    // We don't want to see the updated message in console. We want to see the updated result as resonse.
+    // console.log(result);
+
+    // if user with that :id is not found to update:
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: result.rows[0],
+      });
+    }
+
+    // if user with :id is found to update
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: true,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+// Delete user by DELETE method from db
+const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    // separated the DB query into the user.service module
+    const result = await userService.deleteUserFromDB(id as string);
+
+    // if user with that :id is not found to update:
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: result.rows[0],
+      });
+    }
+
+    // if user with :id is found to update
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: {},
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: true,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
 export const userController = {
   createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -6,4 +6,12 @@ const router = Router();
 
 router.post("/", userController.createUser);
 
+router.get("/", userController.getAllUsers);
+
+router.get("/:id", userController.getSingleUser);
+
+router.put("/:id", userController.updateUser);
+
+router.delete("/:id", userController.deleteUser);
+
 export const userRoute = router;
