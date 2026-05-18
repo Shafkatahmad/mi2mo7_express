01fcbc997a90810cb747927fb20e7d247da5e0f7
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-18 23:11:13 +0600

    added validatoin in authGuard

--- src/modules/middleware/auth.ts
+++ src/modules/middleware/auth.ts
@@ -1,5 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
-
+import jwt, { decode, type DecodeOptions, type JwtPayload } from "jsonwebtoken";
+import config from "../../config";
+import { pool } from "../../db";
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
     // console.log("This is Protected Route");
@@ -12,6 +14,38 @@ const auth = () => {
         message: "Unauthorized access!",
       });
     }
+
+    const decoded = jwt.decode(
+      token as string,
+      config.secret as DecodeOptions,
+    ) as JwtPayload;
+    // console.log(decoded);
+
+    const userData = await pool.query(
+      `
+      SELECT * FROM users WHERE email = $1
+      `,
+      [decoded.email],
+    );
+    // console.log(userData);
+
+    const user = userData.rows[0];
+    // console.log(user);
+
+    if (userData.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!user.is_active) {
+      res.status(403).json({
+        success: false,
+        message: "Forbidden! User is not active.",
+      });
+    }
+
     next();
   };
 };
